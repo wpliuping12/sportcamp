8f2e0725e1edf9c8830ef85310f95d404d14c265
unknown <>
2016-11-01 20:32:14 +0800

    Initial commit

---
+++ caches/configs/database.php
@@ -0,0 +1,19 @@
+<?php
+
+return array (
+	'default' => array (
+		'hostname' => 'localhost',
+		'port' => 3306,
+		'database' => 'sportcamp',
+		'username' => 'root',
+		'password' => 'root',
+		'tablepre' => 'camp',
+		'charset' => 'utf8',
+		'type' => 'mysqli',
+		'debug' => true,
+		'pconnect' => 0,
+		'autoconnect' => 0
+		),
+);
+
+?>
---
+++ phpsso_server/caches/configs/database.php
@@ -0,0 +1,19 @@
+<?php
+
+return array (
+	'default' => array (
+		'hostname' => 'localhost',
+		'port' => 3306,
+		'database' => 'sportcamp',
+		'username' => 'root',
+		'password' => 'root',
+		'tablepre' => 'campsso_',
+		'charset' => 'utf8',
+		'type' => 'mysqli',
+		'debug' => true,
+		'pconnect' => 0,
+		'autoconnect' => 0
+		)
+);
+
+?>
